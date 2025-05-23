6f0d33c54b027bae9fcd2a7fcc15f9640f713c42
Luis Henrique <luisenriklh1177027@gmail.com>
2025-05-23 12:47:34 -0300

    fist commit

---
+++ backend/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const mySql = require('mysql');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const dataBase = mySql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'adotaqui'
+});
+
+// Teste de conexão com o banco de dados
+
+dataBase.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:😔', err);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso!🛜');
+    }
+);
+
+// Rota para obter todos os pets
+app.get('/pets', (req, res) => {
+  const sql = 'SELECT * FROM pets';
+  dataBase.query(sql, (err, result) => {
+    if (err) {
+      console.error('Erro ao buscar os pets:', err);
+      return res.status(500).json({ error: 'Erro ao buscar os pets' });
+    }
+    res.json(result);
+  });
+});
+
+// Rota para cadastra um novo pet
+app.post('/pets', (req, res) => {
+  const { id, name, breed, type_of_animal, description ,adopted } = req.body;
+  const sql = 'INSERT INTO pets (id, name, breed, type_of_animal, description, adopted) VALUES (?, ?, ?, ?, ?, ?)';
+  dataBase.query(sql, [id, name, breed, type_of_animal, description, adopted], (err, result) => {
+    if (err) {
+      console.error('Erro ao cadastrar o pet:', err);
+      return res.status(500).json({ error: 'Erro ao cadastrar o pet' });
+    }
+    result.status(201).json({ message: 'Pet cadastrado com sucesso!' });
+  });
+});
+
+// Outras rotas podem ser adicionadas aqui
+
+
+// Iniciar servidor
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000!🚀');
+});
+
+
+
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Vamos começar a desenvolver o frontend do projeto AdotaAqui!🐶</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
